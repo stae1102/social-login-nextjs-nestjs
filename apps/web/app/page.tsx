ec2a16c81d745b30aa493bfbb667e27c3b2222b0
Seongtae <stae1102@gmail.com>
2023-05-08 06:56:10 +0900

    feat: Get user data and do signup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,17 +3,27 @@
 import { Button, Header } from 'ui';
 import { auth } from './firebase';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useEffect } from 'react';
 
 export default function Page() {
+  const [user, setUser] = useAuthState(auth);
   const googleAuth = new GoogleAuthProvider();
   const login = async () => {
     const result = await signInWithPopup(auth, googleAuth);
   };
 
+  useEffect(() => {
+    // TODO: Fetch POST to the server for saving user data
+  }, [user]);
+
   return (
     <>
       <Header text='Web' />
       <Button onClick={login} content={'Login'} />
+      <div onClick={() => auth.signOut()}>
+        {user ? `Welcome ${user.displayName}` : ''}
+      </div>
     </>
   );
 }
