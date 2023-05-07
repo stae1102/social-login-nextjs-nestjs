dc91c86195186b0b97b1c82e75cd83e81d82b806
Seongtae <stae1102@gmail.com>
2023-05-07 20:30:18 +0900

    feat: Set firebase auth setting

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,41 @@
-import { Button, Header } from "ui";
+'use client';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import { useEffect, useState } from 'react';
+
+import { Button, Header } from 'ui';
+
+const firebaseConfig = {
+  apiKey: 'your api key',
+  authDomain: 'your credentials',
+  projectId: 'your credentials',
+  storageBucket: 'your credentials',
+  messagingSenderId: 'your credentials',
+  appId: 'your credentials',
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+const signIn = () => auth.signInWithPopup(provider);
+const signOut = () => auth.signOut();
 
 export default function Page() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(async (user) => {
+      setUser(user);
+    });
+  }, []);
+
   return (
     <>
-      <Header text="Web" />
+      <Header text='Web' />
       <Button />
     </>
   );
