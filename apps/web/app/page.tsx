e37f8981546f0305a94d1be01bca428be3ec1259
Seongtae <stae1102@gmail.com>
2023-05-07 23:54:25 +0900

    move: Divide firebase setting to single file

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,37 +1,11 @@
 'use client';
 
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import { useEffect, useState } from 'react';
-
 import { Button, Header } from 'ui';
-
-const firebaseConfig = {
-  apiKey: 'your api key',
-  authDomain: 'your credentials',
-  projectId: 'your credentials',
-  storageBucket: 'your credentials',
-  messagingSenderId: 'your credentials',
-  appId: 'your credentials',
-};
-
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-
-const signIn = () => auth.signInWithPopup(provider);
-const signOut = () => auth.signOut();
+import { auth } from './firebase';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 export default function Page() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(async (user) => {
-      setUser(user);
-    });
-  }, []);
+  const googleAuth = new GoogleAuthProvider();
 
   return (
     <>
