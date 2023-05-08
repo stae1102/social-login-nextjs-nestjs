12f6b46289500d585e898f14c982a98ff14e54ae
Seongtae <stae1102@gmail.com>
2023-05-09 07:28:39 +0900

    feat: Initialize firebase app in module layer

--- apps/server/src/modules/config/firebase/firebase.module.ts
+++ apps/server/src/modules/config/firebase/firebase.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { initializeApp } from 'firebase-admin/app';
 
 @Module({
   imports: [ConfigModule],
   providers: [],
   exports: [],
 })
-export class FirebaseModule {}
+export class FirebaseModule implements OnModuleInit {
+  constructor(private readonly configService: ConfigService) {}
+
+  async onModuleInit() {
+    const firebaseConfig = {
+      apiKey: this.configService.get('FIREBASE_API_KEY'),
+      projectId: this.configService.get('FIREBASE_PROJECT_ID'),
+      clientEmail: this.configService.get('FIREBASE_CLIENT_EMAIL'),
+    };
+    initializeApp(firebaseConfig);
+  }
+}
--- apps/server/src/modules/config/firebase/firebase.service.ts
+++ apps/server/src/modules/config/firebase/firebase.service.ts
@@ -1,21 +1,15 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
 @Injectable()
-export class FirebaseService implements OnModuleInit {
+export class FirebaseService {
   constructor(private readonly configService: ConfigService) {}
   private readonly firebaseConfig = {
     apiKey: this.configService.get('FIREBASE_API_KEY'),
     projectId: this.configService.get('FIREBASE_PROJECT_ID'),
     clientEmail: this.configService.get('FIREBASE_CLIENT_EMAIL'),
   };
-
-  async onModuleInit() {
-    initializeApp(this.firebaseConfig);
-    return getAuth();
-  }
 
   async verifyIdToken(idToken: string) {
     const decodedToken = await getAuth().verifyIdToken(idToken);
