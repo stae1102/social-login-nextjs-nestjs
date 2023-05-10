5c31572b3496b3df8a409ac984aeb6d21ce74470
Seongtae <stae1102@gmail.com>
2023-05-11 06:54:37 +0900

    feat: Create Header

---
+++ apps/web/app/Header.tsx
@@ -0,0 +1,44 @@
+import { signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
+
+const Header = () => {
+  const { data } = useSession();
+
+  console.log(data);
+
+  return (
+    <nav>
+      <div>
+        <div>
+          <a href='#'>Next.13 Authentication</a>
+        </div>
+
+        <div>
+          {data && data.user?.image ? (
+            <>
+              <span>
+                <img
+                  src={data.user.image}
+                  height={25}
+                  width={25}
+                  alt='user image'
+                />
+                Hi, {data.user?.name}
+              </span>
+
+              <span style={{ cursor: 'pointer' }} onClick={() => signOut()}>
+                {' '}
+                Logout
+              </span>
+            </>
+          ) : (
+            <span>
+              {' '}
+              <Link href='/login'>Login</Link>
+            </span>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
