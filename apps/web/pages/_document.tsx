80fbdbe1707c46f2ac358d7219d6e76d652cffc5
Seongtae <stae1102@gmail.com>
2023-05-10 23:28:43 +0900

    feat: Create _document.tsx in pages

---
+++ apps/web/pages/_document.tsx
@@ -0,0 +1,11 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html lang='ko'>
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
