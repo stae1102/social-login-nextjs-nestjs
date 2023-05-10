4b66eb25d1b56686837549d371c41d0645551c43
Seongtae <stae1102@gmail.com>
2023-05-10 23:28:17 +0900

    move: Move to pages

--- apps/web/pages/api/auth/[...nextauth].ts
+++ apps/web/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,9 @@ import { AuthOptions } from 'next-auth';
 import NextAuth from 'next-auth/next';
 
 export const authOptions: AuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_GOOGLE_CLIENT_ID,
