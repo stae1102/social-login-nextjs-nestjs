8c9d75d398c7f568ee964bffbda74bb857ed6b00
Seongtae <stae1102@gmail.com>
2023-05-10 18:56:45 +0900

    feat: Add user signup logic

--- apps/server/src/apps/server/auth/auth.service.ts
+++ apps/server/src/apps/server/auth/auth.service.ts
@@ -1,7 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { ReqFirebaseUser } from '../types/request-firebase-user.type';
+import { UserRepository } from '../repositories/user.repository';
+import { Provider } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
-  signup(user: ReqFirebaseUser) {}
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async signup(firebaseUser: ReqFirebaseUser) {
+    const { uid, firebaseId } = firebaseUser;
+
+    const exUser = await this.userRepository.findUserByUid(uid);
+    if (exUser) {
+      throw new ConflictException();
+    }
+
+    await this.userRepository.createUser({
+      nickname: 'test',
+      email: 'test@test.com',
+      provider: Provider.GOOGLE,
+      fcmToken: 'test',
+      uid,
+      firebaseId,
+    });
+  }
 }
